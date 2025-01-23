297d14800df41f933575577fddaceb472b78ae77
CaroDiego <dicafre1404@gmail.com>
2025-01-23 13:16:00 +0100

    Parametros y useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import PokemonsPage from "./pages/PokemonsPage";
+import PokemonPage from "./pages/PokemonPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/pokemons" element={<PokemonsPage />} />
+      <Route path="/pokemon/:id" element={<PokemonPage/>}/>
     </Routes>
   );
 }
