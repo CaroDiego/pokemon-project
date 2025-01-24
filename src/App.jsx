fdac6717dcf82fc5dbbe69e7d956aa48efe6ae57
CaroDiego <dicafre1404@gmail.com>
2025-01-24 11:11:23 +0100

    Code Splitting y Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
-import PokemonsPage from "./pages/PokemonsPage";
-import PokemonPage from "./pages/PokemonPage";
-import ErrorPage from "./pages/ErrorPage";
+import { lazy, Suspense } from "react";
+// import PokemonsPage from "./pages/PokemonsPage";
+// import PokemonPage from "./pages/PokemonPage";
+// import ErrorPage from "./pages/ErrorPage";
+
+//*Sirve para que no se cargue todo al inicio, sino que se cargue solo cuando se necesite
+//!Hacer en proyectos grandes
+const PokemonsPage = lazy(() => import("./pages/PokemonsPage"));
+const PokemonPage = lazy(() => import("./pages/PokemonPage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/pokemons" element={<PokemonsPage />} />
-      <Route path="/pokemon/:id" element={<PokemonPage />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/pokemons" element={<PokemonsPage />} />
+        <Route path="/pokemon/:id" element={<PokemonPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
