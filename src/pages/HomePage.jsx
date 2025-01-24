56a2290bb7a3e8b503450867bdcec058c15e32f8
CaroDiego <dicafre1404@gmail.com>
2025-01-24 11:25:24 +0100

    Virtualizacion de listas

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,24 @@
 import "./HomePage.css";
 import logo from "../assets/pokemon-logo.png";
 import { Link } from "react-router-dom";
+import { FixedSizeList } from "react-window"; //*Solo si hay muchos elementos
+
+const data = Array.from({ length: 100000 }, (_, index) => `Item ${index}`);
 
 function Homepage() {
+  const renderRow = ({ index, style }) => (
+    <div
+      style={{
+        ...style,
+        display: "flex",
+        alignItems: "center",
+        borderBottom: "1px solid lightgray",
+      }}
+    >
+      <span>{data[index]}</span>
+    </div>
+  );
+
   return (
     <section id="home-page">
       <h1 className="title">Bienvenido</h1>
@@ -10,6 +26,16 @@ function Homepage() {
       <Link to="/pokemons" className="link">
         Entrar
       </Link>
+      <div>
+        <FixedSizeList
+          height={400}
+          width={400}
+          itemCount={data.length}
+          itemSize={40}
+        >
+          {renderRow}
+        </FixedSizeList>
+      </div>
     </section>
   );
 }
